bba3b68cfc88ce348d649effc6152291edf37a7b
Leandro León <leandro@theagilemonkeys.com>
2021-04-08 10:51:17 +0100

    Add method to get a string with scores formated

--- custom_utils.py
+++ custom_utils.py
@@ -1,4 +1,18 @@
+import numpy as np
+
+
 def print_on_file(text, filename='results.txt'):
     file = open(filename, 'a+')
     print(text, file=file)
     file.close()
+
+
+def format_scores(scores):
+    result = 'Accuracy: %.4f (%.4f)' % (np.mean(scores['test_accuracy']), np.std(scores['test_accuracy']))
+    result += '\nPrecision: %.4f (%.4f)' % (np.mean(scores['test_precision_weighted']),
+                                            np.std(scores['test_precision_weighted']))
+    result += '\nRecall: %.4f (%.4f)' % (np.mean(scores['test_recall_weighted']),
+                                         np.std(scores['test_recall_weighted']))
+    result += 'F1 Score: %.4f (%.4f)' % (np.mean(scores['test_f1_weighted']),
+                                         np.std(scores['test_f1_weighted']))
+    return result
